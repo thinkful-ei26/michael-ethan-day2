64a3967d092701458a24e0735cd556a9a0b40000
Michael McKay <mamckay89@gmail.com>
2018-10-30 10:11:35 -0600

    Functions as arguements

--- main.js
+++ main.js
@@ -1,54 +1,73 @@
 'use strict';
 
-function maxNum(num) {
+// function maxNum(num) {
     
-  if(num.length === 0){
-    return 0;
-  }
-  let index = num.length;
-  let currentMax = 0;
-  while (index != -1) {
+//   if(num.length === 0){
+//     return 0;
+//   }
+//   let index = num.length;
+//   let currentMax = 0;
+//   while (index != -1) {
         
-    if(num[index] > currentMax ){
-      currentMax = num[index];
-    }
-    --index;
-  }
+//     if(num[index] > currentMax ){
+//       currentMax = num[index];
+//     }
+//     --index;
+//   }
     
-  return currentMax;
+//   return currentMax;
 
-}
+// }
 
-function minNum(num) {
-  if(num.length === 0){
-    return 0;
-  }
-  let index = num.length;
-  let currentMin = 10;
-  while (index != -1) {
+// function minNum(num) {
+//   if(num.length === 0){
+//     return 0;
+//   }
+//   let index = num.length;
+//   let currentMin = 10;
+//   while (index != -1) {
         
-    if(num[index] < currentMin ){
-      currentMin = num[index];
-    }
-    --index;
-  }
+//     if(num[index] < currentMin ){
+//       currentMin = num[index];
+//     }
+//     --index;
+//   }
     
-  return currentMin;
+//   return currentMin;
+// }
+
+
+// console.log(maxNum([1,2,3]));
+// console.log(minNum([1,2,3]));
+
+
+// function average(num) {
+//   // return num.reduce((num, total) => num + total)
+//   let runningTotal = 0;
+//   num.forEach(function(element){
+//     runningTotal += element;
+//   });
+//   return runningTotal/num.length;
+// }
+
+// console.log(average([1,2,3]));
+
+
+function repeat(fn,n) {
+
+  for (let index = 0; index < n; index++) {
+
+    fn();
+
+  }
 }
 
-
-console.log(maxNum([1,2,3]));
-console.log(minNum([1,2,3]));
-
-
-function average(num) {
-  // return num.reduce((num, total) => num + total)
-  let runningTotal = 0;
-  num.forEach(function(element){
-    runningTotal += element;
-  });
-  return runningTotal/num.length;
+function hello() {
+  console.log("hello world");
+}
+function goodbye() {
+  console.log("Goodbye World")
 }
 
-console.log(average([1,2,3]));
-
+repeat(hello,5);
+repeat(goodbye,5);
